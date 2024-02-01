50f7d2361514da57ef2e54aa948d1e13bbfb503f
dutch <dutch@Macs-MacBook-Pro.local>
2024-02-01 11:06:46 +0700

    init

--- lib/cdk-app-stack.ts
+++ lib/cdk-app-stack.ts
@@ -1,6 +1,12 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as ecs from 'aws-cdk-lib/aws-ecs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as tasks from 'aws-cdk-lib/aws-stepfunctions-tasks';
+import * as stepfunctions from 'aws-cdk-lib/aws-stepfunctions';
+import * as events from 'aws-cdk-lib/aws-events';
+import * as targets from 'aws-cdk-lib/aws-events-targets';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
 
 export class CdkAppStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -8,9 +14,97 @@ export class CdkAppStack extends cdk.Stack {
 
     // The code that defines your stack goes here
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkAppQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
+    const vpc = new ec2.Vpc(this, 'TestVPC', {
+      maxAzs: 2,
+    });
+
+    const ecsCluster = new ecs.Cluster(this, 'TestECSCluster', {
+      vpc,
+    });
+
+    const taskDefinition = new ecs.FargateTaskDefinition(this, 'TestTaskDefinition');
+
+    const container = taskDefinition.addContainer('TestContainer', {
+      image: ecs.ContainerImage.fromRegistry('nginx'),
+      memoryLimitMiB: 256,
+    });
+
+    container.addPortMappings({ containerPort: 80 });
+
+
+    // const ecsService = new ecs.Ec2Service(this, 'TestECSService', {
+    //   cluster: ecsCluster,
+    //   taskDefinition,
     // });
+
+
+    const rule = new events.Rule(this, 'EcsScheduledRule', {
+      schedule: events.Schedule.expression('rate(1 minute)'),
+    });
+
+
+    const runTask = new tasks.EcsRunTask(this, 'RunECSTask', {
+      integrationPattern: stepfunctions.IntegrationPattern.RUN_JOB,
+      cluster: ecsCluster,
+      taskDefinition: taskDefinition,
+      launchTarget: new tasks.EcsFargateLaunchTarget(),
+      // containerOverrides: [
+      // ]
+    })
+
+    const retry = runTask.addRetry({
+      errors: ['ECS.AccessDeniedException'],
+      interval: cdk.Duration.seconds(60),
+      maxAttempts: 2,
+      backoffRate: 2
+    })
+
+    const notifyFailure = new tasks.LambdaInvoke(this, 'NotifyFailure', {
+      lambdaFunction: this.createLambdaNotifyFailure(),
+      outputPath: '$.taskError',
+      payloadResponseOnly: true
+    })
+
+    // const catchAll = runTask.addCatch(notifyFailure, {
+    //   errors: ['States.ALL'],
+    //   resultPath: '$.taskError'
+    // })
+
+    const definition =  runTask
+    .addCatch(notifyFailure, {
+      errors:['States.ALL'],
+      resultPath: '$.taskError',
+    });
+
+    // const defination = stepfunctions.Chain.start(runTask).next(retry).next(catchAll)
+
+    const stepFunctions = new stepfunctions.StateMachine(this, 'TestStateMachineCDK', {
+      stateMachineName: 'test-retry',
+      timeout: cdk.Duration.minutes(2),
+      definition: definition
+    })
+
+    rule.addTarget(new targets.SfnStateMachine(stepFunctions));
   }
+
+  createLambdaNotifyFailure() {
+
+
+    const path = require('path');
+
+    const lambdaFunctionName = `lambda-notify-failure-ecs-scheduled-task`;
+    return new lambda.Function(this, `NotifyFailureReTryEcsTaskLambda`, {
+        runtime: lambda.Runtime.NODEJS_18_X,
+        functionName: lambdaFunctionName,
+        handler: 'index.handler',
+        code: lambda.Code.fromAsset(
+            path.join(__dirname, '..','lib', 'lambda', 'notify-failure-ecs-scheduled-task')
+        ),
+        environment: { SLACK_WEBHOOK_URL: 'hook' },
+        timeout: cdk.Duration.minutes(1),
+        memorySize: 128,
+    });
+ }
+ 
+  
 }
